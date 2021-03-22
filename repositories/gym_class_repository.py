45d4f2e0a6646ffbd2653e364d6b86d5ebfe380a
StuMcA <stuart.mcara@outlook.com>
2021-03-22 10:32:37 +0000

    added update to classes repo

--- repositories/gym_class_repository.py
+++ repositories/gym_class_repository.py
@@ -34,3 +34,14 @@ def select(id):
         class_found = GymClass(result["class_type"], result["class_date"], result["class_time"], result["instructor"], result["duration"], result["class_locatin"], result["capacity"], result["id"])
 
     return class_found
+
+def update(gym_class):
+    sql = """
+        UPDATE gym_classes
+        SET (class_type, class_date, class_time, instructor, duration, class_location, capacity) = (%s, %s, %s, %s, %s, %s, %s)
+        WHERE id = %s
+    
+    """
+    values = [gym_class.class_type, gym_class.date, gym_class.time, gym_class.instructor, gym_class.duration, gym_class.location, gym_class.capacity]
+    result = run_sql(sql, values)
+    return result

ece348f6735b70ea35c6b0a6cef4a5259cf2f43b
StuMcA <stuart.mcara@outlook.com>
2021-03-22 10:55:53 +0000

    added class show route

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -7,3 +7,8 @@ gym_class_blueprint = Blueprint("gym_class", __name__)
 def classes():
     gym_classes = gym_class_repository.select_all()
     return render_template('gym_classes/index.html', title="Classes", classes=gym_classes)
+
+@gym_class_blueprint.route('/classes/<id>')
+def gym_class(id):
+    gym_class = gym_class_repository.select(id)
+    return render_template('gym_classes/show.html', title=f"{gym_class.class_type} with {gym_class.instructor}", gym_class=gym_class)

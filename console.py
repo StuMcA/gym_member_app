94a63e15e40f051f764132fcd5a5e237c5843521
StuMcA <stuart.mcara@outlook.com>
2021-03-22 10:14:44 +0000

    classes data seeded to db

--- console.py
+++ console.py
@@ -1,5 +1,7 @@
 import repositories.member_repository as member_repository
+import repositories.gym_class_repository as gym_class_repository
 from models.member import Member
+from models.gym_class import GymClass
 
 member_repository.delete_all()
 
@@ -8,3 +10,11 @@ member_2 = Member("Michael", "Scott", '1965-12-03', "Off-peak")
 
 member_repository.save(member_1)
 member_repository.save(member_2)
+
+class_1 = GymClass("Weights", "Big Jane", "23/03/2021", "14:00", 90, "Weights Studio", 15)
+class_2 = GymClass("Yoga", "Bendy Jo", "23/03/2021", "13:30", 60, "Yoga Studio", 10)
+
+gym_class_repository.save(class_1)
+gym_class_repository.save(class_2)
+
+

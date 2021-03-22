47ffde6722a9509350e47cfbf219c354ba78f98f
StuMcA <stuart.mcara@outlook.com>
2021-03-22 10:09:08 +0000

    added save class to repo

--- models/gym_class.py
+++ models/gym_class.py
@@ -1,4 +1,4 @@
-class Gym_class:
+class GymClass:
     def __init__(self, class_type, instructor, date, time, duration, location, capacity, id = None ):
         self.class_type = class_type
         self.instructor = instructor
--- repositories/gym_class_repository.py
+++ repositories/gym_class_repository.py
@@ -1,16 +1,15 @@
 from db.run_sql import run_sql
-from models.gym_class import Class
+from models.gym_class import GymClass
 
 def save(gym_class):
     sql = """
         INSERT INTO gym_classes
-        
+        (class_type, class_date, class_time, duration, class_location, capacity) 
+        VALUES (%s, %s, %s, %s, %s, %s)
+        RETURNING id
     """
-    gym_class = Gym_class(gym_class.class_type, 
-                          gym_class.instructor, 
-                          gym_class.date, 
-                          gym_class.time, 
-                          gym_class.duration, 
-                          gym_class.location, 
-                          gym_class.capacity
-                          )
+    values = [gym_class.instructor, gym_class.date, gym_class.time, gym_class.duration, gym_class.location, gym_class.capacity]
+    result = run_sql(sql, values)
+
+    gym_class.id = result[0]['id']
+                          

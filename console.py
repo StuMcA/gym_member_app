ee60542f14841f25040c6c1f62ac53ff72fe4129
StuMcA <stuart.mcara@outlook.com>
2021-03-23 12:09:30 +0000

    more css

--- console.py
+++ console.py
@@ -17,7 +17,7 @@ location_repository.delete_all()
 
 
 member_1 = Member("Stuart", "McAra", '1989-03-26', "Peak")
-member_2 = Member("Michael", "Scott", '1965-12-03', "Off-peak")
+member_2 = Member("Michael", "Scott", '1965-12-03', "Off-Peak")
 
 member_repository.save(member_1)
 member_repository.save(member_2)
